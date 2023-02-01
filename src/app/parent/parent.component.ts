29035a179d01a4daffc04373beb3fe66cda302a6
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-02-01 15:24:38 +0700

    angular-date-picker

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css'],
+})
+export class ParentComponent implements OnInit {
+  locale: string = 'en';
+  selectedDate: any;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  setSelectedDate(date: any) {
+    this.selectedDate = date;
+  }
+
+  canChangeMonthLogic(num: any, currentDate: any) {
+    currentDate.add(num, 'month');
+    const minDate = moment().add(-1, 'month');
+    const maxDate = moment().add(1, 'year');
+
+    return currentDate.isBetween(minDate, maxDate);
+  }
+
+  isAvailableLogic(dateToCheck: any) {
+    if (dateToCheck.isBefore(moment(), 'day')) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
